5d700a8896466a2eac03d4530cbfb89847a9a7a0
JinHyung <ppjh8263@naver.com>
2021-02-23 23:28:18 +0900

    login api

--- login/views.py
+++ login/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import LoginUser
 from django.contrib.auth.hashers import  make_password,check_password
-
+from .serialize import LoginUserSerializer
 # Create your views here.
 
 class AppLogin(APIView):
@@ -16,32 +16,28 @@ class AppLogin(APIView):
             return Response(dict(msg="해당 사용자가 없습니다."))
 
         if check_password(user_pw, user.user_pw):
-            return Response(dict(msg="로그인 성공"))
+            return Response(dict(msg="로그인 성공",
+                                 user_id=user.user_id,
+                                 birth_day=user.birth_day,
+                                 gender=user.gender,
+                                 email=user.email,
+                                 name=user.name,
+                                 age=user.age,))
         else:
             return Response(dict(msg="로그인 실패, 비밀번호 틀림!"))
 
 
 class RegistUser(APIView):
     def post(self, request):
-        user_id = request.data.get('user_id')
-        user_pw = request.data.get('user_pw')
-        user_pw_encryted = make_password(user_pw)
+        serializer = LoginUserSerializer(request.data)
 
-        # if user_id = 특수문자, 숫자, 한글
+        if LoginUser.objects.filter(user_id=serializer.data['user_id']).exist():
+            user = LoginUser.objects.filter(user_id=serializer.data['user_id']).first
+            data = dict(msg="동일한 ID가 있습니다.",
+                        user_id=user.user_id,
+                        user_pw=user.user_pw)
 
-        user = LoginUser.objects.filter(user_id=user_id).first()
+            return Response(data)
 
-
-
-        if user is not None:
-            return Response(dict(msg="동일한 ID가 있습니다."))
-
-        LoginUser.objects.create(user_id=user_id,
-                                 user_pw=user_pw_encryted)
-
-        data = dict(
-            user_id=user_id,
-            user_pw=user_pw_encryted
-        )
-
-        return Response(data)
+        user = serializer.create(request.data)
+        return Response(LoginUserSerializer(user).data)
