50c71e640ae67b900b03f5d8203f58810404b8f9
JinHyung <ppjh8263@naver.com>
2021-03-05 15:32:27 +0900

    0305

--- todo/urls.py
+++ todo/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     url('create', views.TaskCreate.as_view(), name='create'),
     url('select', views.TaskSelect.as_view(), name='select'),
+    url('toggle', views.TaskToggle.as_view(), name='toggle'),
+    url('delete', views.TaskDelete.as_view(), name='delete'),
     url('', views.Todo.as_view(), name='todo'),
 ]
+
--- todo/views.py
+++ todo/views.py
@@ -5,6 +5,18 @@ from datetime import datetime
 from django.shortcuts import render
 
 
+class TaskSelect(APIView):
+    def post(self, request):
+        tasks = Task.objects.all()
+        task_list=[]
+        for task in tasks:
+            task_list.append(dict(id=task.id,
+                                  name=task.name,
+                                  done=task.done))
+
+        return Response(status=200, data=dict(tasks=task_list))
+    # return Response(status=400, data=dict(code=100003)) 직접 정의한 에러코드 사용법
+
 
 class Todo(APIView):
     def post(self, request):
@@ -43,30 +55,41 @@ class Todo(APIView):
 
 class TaskCreate(APIView):
     def post(self, request):
-        user_id = request.data.get('user_id', "")
-        name = request.data.get('name', "")
-        end_date = request.data.get('end_date', None)
-        if end_date:
-            end_date = datetime.strptime(end_date, '%Y-%m-%d').date()
-            task = Task.objects.create(user_id=user_id, name=name, end_date=end_date)
+        user_id = request.data.get('user_id', '')
+        todo_id = request.data.get('todo_id','')
+        name = request.data.get('name', '')
 
-            return Response(
-                dict(msg="To-Do 생성 완료",
-                     name=task.name,
-                     start_date=task.start_date.strftime('%Y-%m-%d'),
-                     end_date=task.end_date))
+        task = Task.objects.create(id=todo_id, user_id=user_id, name=name)
 
+        return Response()
+        # return Response(data=dict(task=task))
+        #
+        # end_date = request.data.get('end_date', None)
+        # if end_date:
+        #     end_date = datetime.strptime(end_date, '%Y-%m-%d').date()
+        #     task = Task.objects.create(user_id=user_id, name=name, end_date=end_date)
+        #
+        #     return Response(
+        #         dict(msg="To-Do 생성 완료",
+        #              name=task.name,
+        #              start_date=task.start_date.strftime('%Y-%m-%d'),
+        #              end_date=task.end_date))
 
-class TaskSelect(APIView):
-    def post(self, request):
-        user_id = request.data.get('user_id', "")
-        tasks = Task.objects.filter(user_id=user_id)
-        task_list = []
-        for task in tasks:
-            task_list.append(
-                dict(name=task.name,
-                     start_date=task.start_date,
-                     end_date=task.end_date,
-                     state=task.state))
+class TaskToggle(APIView):
+    def post(self, resquest):
+        todo_id = resquest.data.get('todo_id','')
+        task = Task.objects.get(id=todo_id)
+        if task:
+            task.done = False if task.done is True else True
+            task.save()
 
-        return Response(dict(tasks=task_list))
+        return Response()
+
+class TaskDelete(APIView):
+    def post(self, resquest):
+        todo_id = resquest.data.get('todo_id','')
+        task = Task.objects.get(id=todo_id)
+        if task:
+            task.delete()
+
+        return Response()
